132211410822bead1206dc062802a896c69d4dd7
Yaroslav Sochka <web@sochka.com>
2023-05-06 14:36:45 +0300

    Update comment_ChatGPT.php
    Добавлена возможность генерации комментария GPT к выбранным записям на странице записей. 
    Перейдите в Консоль - Записи - Все записи (в административной панели WordPress).
    Выберите нужные записи, используя флажки, расположенные слева от каждой записи.
    Нажмите на выпадающий список "Действия", который расположен над таблицей записей.
    Выберите действие из списка "GPT комментарий"

--- comment_ChatGPT.php
+++ comment_ChatGPT.php
@@ -3,7 +3,7 @@
 Plugin Name: WP Комментатор ChatGPT
 Plugin URI: https://sochka.com
 Description: Искусственный интеллект (ChatGPT) оставляет осмысленный комментарий к вашей записи (каждый раз: при создания новой, при редактировании старой). Дополняет новость уникальным контентом! Стимулирует дальнейшую дискуссию читателями! Настройте: НАСТРОЙКИ - ОБСУЖДЕНИЯ...
-Version: 0.2
+Version: 0.3
 Author: Yaroslav Sochka
 Author URI: https://sochka.com
 License: GPLv2 or later
@@ -11,44 +11,37 @@ License URI: https://www.gnu.org/licenses/gpl-2.0.html
 */
 
 //настройки плагина
-
 add_action( 'admin_init', 'gpt_settings_api_init' );
 
 function gpt_settings_api_init() {
-
-	// блок опций на базовую страницу "Обсуждение"
 	add_settings_section(
-		'gpt_setting_section', // секция
+		'gpt_setting_section',
 		'Настройки комментатора GPT',
 		'gpt_setting_section_callback_function',
-		'discussion' // страница
+		'discussion'
 	);
-
-	//поля опций. Указываем название, описание,
-
 	add_settings_field(
 		'gpt_setting_name',
 		'API ключ GPT',
 		'gpt_setting_callback_function',
-		'discussion', // страница
-		'gpt_setting_section' // секция
+		'discussion',
+		'gpt_setting_section'
 	);
 	add_settings_field(
 		'gpt_setting_name2',
 		'Email от имени которого будут публиковаться комментарии',
 		'gpt_setting_callback_function2',
-		'discussion', // страница
-		'gpt_setting_section' // секция
+		'discussion',
+		'gpt_setting_section'
 	);
 		add_settings_field(
 		'gpt_setting_name3',
 		'Имя комментатора',
 		'gpt_setting_callback_function3',
-		'discussion', // страница
-		'gpt_setting_section' // секция
+		'discussion',
+		'gpt_setting_section'
 	);
 
-	// Регистрируем опции, чтобы они сохранялись при отправке
 	register_setting( 'discussion', 'gpt_setting_name' );
 	register_setting( 'discussion', 'gpt_setting_name2' );
 	register_setting( 'discussion', 'gpt_setting_name3' );
@@ -58,7 +51,6 @@ function gpt_settings_api_init() {
 function gpt_setting_section_callback_function() {
 	echo '<p>Внимательно заполните ВСЕ поля для корректной работы комментатора ChatGPT</p>';
 }
-
 
 function gpt_setting_callback_function() {
 	?>
@@ -91,21 +83,11 @@ function gpt_setting_callback_function3() {
 	<?php
 }
 
-//обращаемся к ИИ за комментарием
-function add_comment_on_post_update( $post_id, $post_after, $post_before ) {
+//генерируем GPT комментарий к записи по id
 
-    // If this is just a revision, don't send the email.
-    if ( wp_is_post_revision( $post_id ) )
-        return;
-    if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE)
-        return;
-    if (isset($post_before->post_status) && 'auto-draft' == $post_before->post_status) {
-        return;
-    }
+function add_comment_to_post($post_id) {
+$inputr = array("Отвечай от имени человека. Прокомментируй новость: ", "Прокомментируй от имени человека. Что ты думаешь по этому поводу: ", "Прокомментируй от имени человека. Как ты к этому относишься: ", "Прокомментируй от имени человека. Хорошо это или плохо: ", "Прокомментируй от имени человека. Есть ли в этом смысл: ");
 
-    if ('post' == $_POST['post_type']) {
-
-$inputr = array("Отвечай от имени человека. Прокомментируй новость: ", "Отвечай от имени человека. Что ты думаешь по этому поводу: ", "Отвечай от имени человека. Как ты к этому относишься: ", "Отвечай от имени человека. Хорошо это или плохо: ", "Отвечай от имени человека. Есть ли в этом смысл: ");
 $fraza = $inputr[array_rand($inputr)];
 		
 $api_key = esc_attr( get_option(  'gpt_setting_name' ) );
@@ -144,31 +126,84 @@ $commentbot = $response['choices'][0]['message']['content'];
 }
 curl_close($ch);
 
-
 $imenos=esc_attr( get_option('gpt_setting_name3'));
 $words = explode(",", $imenos);
 $random_imeno = trim($words[array_rand($words)]);
 
-        $data = array(
-            'comment_post_ID' => $post_id,
-            'comment_author' => $random_imeno,
-            'comment_author_email' => esc_attr( get_option(  'gpt_setting_name2' ) ),
-            'comment_author_url' => '',
-            'comment_content' => $commentbot,
+$data = array(
+    'comment_post_ID' => $post_id,
+    'comment_author' => $random_imeno,
+    'comment_author_email' => esc_attr( get_option(  'gpt_setting_name2' ) ),
+    'comment_author_url' => '',
+    'comment_content' => $commentbot,
 	'comment_parent'       => 0,
 	'user_id'              => '',
 	'comment_author_IP'    => '127.0.0.1',
-	'comment_agent'        => 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.10) Gecko/2009042316 Firefox/3.0.10 (.NET CLR 3.5.30729)',
 	'comment_date' => date('Y-m-d H:i:s'),
 	'comment_approved'     => 1
         );
 	
-		if (strlen($commentbot) > 25) {wp_insert_comment($data);}
-
-
+if (strlen($commentbot) > 15) { //проверяем, не пустой ли комментарий
+$comment_id = wp_insert_comment(wp_slash($data));
+}
+	
+    if ($comment_id) {
+        // Комментарий успешно добавлен
+        return true;
+    } else {
+        // Произошла ошибка при добавлении комментария
+        return false;
     }
+}
+
+
+
+
+//генерируем комментарий в момент СОЗДАНИЯ/СОХРАНЕНИЯ ЗАПИСИ
+function add_comment_on_post_update( $post_id, $post_after, $post_before ) {
+
+	if ( wp_is_post_revision( $post_id ) )
+        return;
+    if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE)
+        return;
+    if (isset($post_before->post_status) && 'auto-draft' == $post_before->post_status) {
+        return;
+    }
+
+ if ('post' == $_POST['post_type']) { //проверяем, что это тип - записи
+add_comment_to_post  ($post_id);
+ }
 
 }
 add_action( 'post_updated', 'add_comment_on_post_update', 10 , 3 );
+
+
+
+
+
+//действие на странице списка записей
+add_filter( 'bulk_actions-edit-post', 'gpt_custom_bulk_actions' );
+
+function gpt_custom_bulk_actions( $actions ) {
+    $actions['gpt_custom_action'] = __( 'GPT комментарий', 'textdomain' );
+    return $actions;
+}
+
+//генерируем комментарий GPT к выбранным записям
+add_action( 'admin_action_gpt_custom_action', 'gpt_custom_bulk_action_handler' );
+
+function gpt_custom_bulk_action_handler() {
+    $post_ids = isset( $_REQUEST['post'] ) ? $_REQUEST['post'] : array(); // получаем выбранные записи
+
+    foreach ( $post_ids as $post_id ) {
+        // генерируем комментарий GPT к каждой выбранной записи
+        add_comment_to_post  ($post_id);
+    }
+    // перенаправление на страницу со списком записей
+    $sendback = remove_query_arg( array( 'action', 'action2' ), wp_get_referer() );
+	wp_redirect( $sendback ); 
+
+    exit();
+}
 
 ?>
